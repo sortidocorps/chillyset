65d898634f369efbd35abe64df56eb11622697f0
Unknown <sortidocorp@gmail.com>
2019-11-19 14:01:34 +0100

    x

--- src/main/java/com/performance/chillyset/FiilObjectConfig.java
+++ src/main/java/com/performance/chillyset/FiilObjectConfig.java
@@ -17,11 +17,14 @@ public class FiilObjectConfig {
     @Value("${active.huge.object}")
     boolean activation;
 
+    @Value("${max.huge.object}")
+    int max;
+
     @EventListener(ApplicationReadyEvent.class)
     public void doSomethingAfterStartup() {
         if (activation) {
             // Fill data base with a lot of objects.
-            for (int i =0; i <= 10000; i++){
+            for (int i =0; i <= max; i++){
                 HugeObject hugeObject = new HugeObject();
                 hugeObject.setId(i);
                 hugeObject.setName("huge_"+i);
